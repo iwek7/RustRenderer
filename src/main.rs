191a473ce5910cd8c5ad7a5a8a6011b56c9e361a
iwek <miwanczuk7@gmail.com>
2021-06-13 23:00:29 +0200

    shaders

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-extern crate sdl2;
 extern crate gl;
+extern crate sdl2;
+
+use std::ffi::{CString};
+
+pub mod render_gl;
 
 fn main() {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 5);
+
     let window = video_subsystem
         .window("Game", 900, 700)
         .opengl()
@@ -17,14 +26,29 @@ fn main() {
     let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
     unsafe {
+        gl::Viewport(0, 0, 900, 700);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
         gl::ClearColor(0.3, 0.3, 0.5, 1.0);
     }
+
+    let vert_shader = render_gl::Shader::from_vert_source(
+        &CString::new(include_str!("triangle.vert")).unwrap()
+    ).unwrap();
+
+    let frag_shader = render_gl::Shader::from_frag_source(
+        &CString::new(include_str!("triangle.frag")).unwrap()
+    ).unwrap();
+
+    let shader_program = render_gl::Program::from_shaders(
+        &[vert_shader, frag_shader]
+    ).unwrap();
+    shader_program.set_used();
 
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit {..} => break 'main,
-                _ => {},
+                sdl2::event::Event::Quit { .. } => break 'main,
+                _ => {}
             }
 
             unsafe {
@@ -33,6 +57,6 @@ fn main() {
 
             window.gl_swap_window();
         }
-
     }
 }
+
