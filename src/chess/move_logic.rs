01de5b47807eb757226613c5002754890ae62a6e
iwek7 <miwanczuk7@gmail.com>
2021-07-29 18:47:45 +0200

    free movement for rook

--- src/chess/move_logic.rs
+++ src/chess/move_logic.rs
@@ -1,6 +1,6 @@
-use crate::chess::infrastructure::PieceType;
 use crate::chess::chessboard::ChessboardState;
 use crate::chess::field::FieldLogic;
+use crate::chess::infrastructure::PieceType;
 
 pub trait PieceMoveComponent {
     fn is_move_allowed(&self, state: &ChessboardState, target_field: &FieldLogic, occupied_field: &FieldLogic) -> bool {
@@ -11,6 +11,7 @@ pub trait PieceMoveComponent {
 }
 
 pub struct PawnMoveComponent {}
+
 impl PieceMoveComponent for PawnMoveComponent {
     fn get_all_allowed_moves(&self, state: &ChessboardState, occupied_field: &FieldLogic) -> Vec<FieldLogic> {
         vec!(
@@ -22,13 +23,31 @@ impl PieceMoveComponent for PawnMoveComponent {
 }
 
 pub struct RockMoveComponent {}
+
 impl PieceMoveComponent for RockMoveComponent {
     fn get_all_allowed_moves(&self, state: &ChessboardState, occupied_field: &FieldLogic) -> Vec<FieldLogic> {
-        todo!()
+        return get_all_fields();
     }
 }
 
-
 pub fn create_move_component(piece_type: &PieceType) -> Box<dyn PieceMoveComponent> {
-    Box::new(PawnMoveComponent {})
+    match piece_type {
+        PieceType::ROOK => Box::new(RockMoveComponent {}),
+        _ => Box::new(PawnMoveComponent {})
+    }
+}
+
+fn get_all_fields() -> Vec<FieldLogic> {
+    let mut vec = vec!();
+    for i in 1..=8 {
+        vec.push(FieldLogic::from_string(format!("A{}", i).as_str()));
+        vec.push(FieldLogic::from_string(format!("B{}", i).as_str()));
+        vec.push(FieldLogic::from_string(format!("C{}", i).as_str()));
+        vec.push(FieldLogic::from_string(format!("D{}", i).as_str()));
+        vec.push(FieldLogic::from_string(format!("E{}", i).as_str()));
+        vec.push(FieldLogic::from_string(format!("F{}", i).as_str()));
+        vec.push(FieldLogic::from_string(format!("G{}", i).as_str()));
+        vec.push(FieldLogic::from_string(format!("H{}", i).as_str()));
+    }
+    return vec;
 }
