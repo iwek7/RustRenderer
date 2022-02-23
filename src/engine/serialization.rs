989be64aaa61e59d15fee9023fd8c74e77f4bfa3
iwek <miwanczuk7@gmail.com>
2022-02-23 17:33:16 +0100

    deserialize additional properties

--- src/engine/serialization.rs
+++ src/engine/serialization.rs
@@ -1,16 +1,19 @@
+use std::iter::Map;
 use std::rc::Rc;
+
 use sdl2::pixels::Color;
-use crate::engine::rendering::material::Material;
-use crate::engine::api::maths::vertex::VertexShaderDataLayout;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+
 use crate::engine::api::drawable::Drawable;
 use crate::engine::api::engine_utilities::EngineUtilities;
 use crate::engine::api::maths::rectangle::Rectangle;
 use crate::engine::api::maths::vertex::ColoredVertexDataLayout;
+use crate::engine::api::maths::vertex::VertexShaderDataLayout;
+use crate::engine::rendering::material::Material;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ProjectDto {
-    objects: Vec<GameObjectDto>
+    objects: Vec<GameObjectDto>,
 }
 
 impl ProjectDto {
@@ -26,7 +29,8 @@ pub struct GameObjectDto {
     object_type: String,
     material: String,
     world_position: (f32, f32, f32),
-    color: (f32, f32, f32, f32)
+    color: (f32, f32, f32, f32),
+    properties: std::collections::HashMap<String, String>,
 }
 
 
@@ -42,10 +46,15 @@ impl GameObjectMapper {
 
     pub fn map_dto_to_game_object(&self, dto: &GameObjectDto) -> Rectangle<ColoredVertexDataLayout> {
         let material = self.engine_utilities.get_resource_manager().fetch_shader_material(&dto.material);
+        let sizeX = dto.properties.get("sizeX").unwrap().parse::<f32>().unwrap();
+        let sizeY = dto.properties.get("sizeY").unwrap().parse::<f32>().unwrap();
+
+
+
         return
             Rectangle::new_colored(
                 &dto.world_position.into(),
-                &glam::vec2(1.0, 1.0),
+                &glam::vec2(sizeX, sizeY),
                 material,
                 dto.color.into(),
             );
